c9a04cdf367fd05dd5b0cdb40c2c7353594e03fd
Rinary <rinary.super@gmail.com>
2026-05-03 20:16:09 +0300

    app

---
+++ Starlight.Launcher/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Starlight.Launcher
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
