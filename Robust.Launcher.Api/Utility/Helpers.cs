807de5a6fc78f544a13e655ef7f1abc221ef95f5
Rinary <rinary.super@gmail.com>
2026-05-19 21:03:45 +0300

    EngineManager

--- Robust.Launcher.Api/Utility/Helpers.cs
+++ Robust.Launcher.Api/Utility/Helpers.cs
@@ -1,6 +1,15 @@
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
+using Mono.Unix;
+using TerraFX.Interop.Windows;
+using Win = TerraFX.Interop.Windows.Windows;
 
 namespace Robust.Launcher.Api.Utility;
 
@@ -8,10 +17,125 @@ public static class Helpers
 {
     public static readonly JsonSerializerOptions JsonWebOptions = new(JsonSerializerDefaults.Web);
 
+    public delegate void DownloadProgressCallback(long downloaded, long total);
+
+    public static void ExtractZipToDirectory(string directory, Stream zipStream)
+    {
+        using var zipArchive = new ZipArchive(zipStream);
+        zipArchive.ExtractToDirectory(directory);
+    }
+
+    public static void ClearDirectory(string directory)
+    {
+        var dirInfo = new DirectoryInfo(directory);
+        foreach (var fileInfo in dirInfo.EnumerateFiles())
+        {
+            fileInfo.Delete();
+        }
+
+        foreach (var childDirInfo in dirInfo.EnumerateDirectories())
+        {
+            childDirInfo.Delete(true);
+        }
+    }
+
     public static async Task<T> AsJson<T>(this HttpContent content) where T : notnull
     {
         var str = await content.ReadAsStringAsync();
         return JsonSerializer.Deserialize<T>(str, JsonWebOptions) ??
                throw new JsonException("AsJson: did not expect null response");
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
+    public static unsafe void MarkDirectoryCompress(string path)
+    {
+        // TODO: Linux: chattr +c
+        if (!OperatingSystem.IsWindows())
+            return;
+
+        fixed (char* pPath = path)
+        {
+            var handle = Win.CreateFileW(
+                pPath,
+                Win.GENERIC_ALL,
+                FILE.FILE_SHARE_READ,
+                null,
+                OPEN.OPEN_EXISTING,
+                FILE.FILE_FLAG_BACKUP_SEMANTICS,
+                HANDLE.NULL);
+
+            var lpBytesReturned = 0u;
+            var lpInBuffer = (short)Win.COMPRESSION_FORMAT_DEFAULT;
+
+            Win.DeviceIoControl(
+                handle,
+                FSCTL.FSCTL_SET_COMPRESSION,
+                &lpInBuffer,
+                sizeof(short),
+                null,
+                0,
+                &lpBytesReturned,
+                null);
+
+            Win.CloseHandle(handle);
+        }
+    }
+
+    public static void EnsureDirectoryExists(string dir)
+    {
+        if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+    }
+
+    public static async Task DownloadToStream(this HttpClient client, string uri, Stream stream,
+        DownloadProgressCallback? progress = null, CancellationToken cancel = default)
+    {
+        await Task.Run(async () =>
+        {
+            using var response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancel);
+            response.EnsureSuccessStatusCode();
+
+            await using var contentStream = await response.Content.ReadAsStreamAsync(cancel);
+            // await using var fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite, 4096, useAsync: true);
+            var totalLength = response.Content.Headers.ContentLength;
+            if (totalLength.HasValue)
+            {
+                progress?.Invoke(0, totalLength.Value);
+            }
+
+            var totalRead = 0L;
+            var reads = 0L;
+            const int bufferLength = 4096;
+            var buffer = ArrayPool<byte>.Shared.Rent(bufferLength);
+            var isMoreToRead = true;
+
+            do
+            {
+                var read = await contentStream.ReadAsync(buffer.AsMemory(0, bufferLength), cancel);
+                if (read == 0)
+                {
+                    isMoreToRead = false;
+                }
+                else
+                {
+                    await stream.WriteAsync(buffer.AsMemory(0, read), cancel);
+
+                    reads += 1;
+                    totalRead += read;
+                    if (totalLength.HasValue && reads % 20 == 0)
+                    {
+                        progress?.Invoke(totalRead, totalLength.Value);
+                    }
+                }
+            } while (isMoreToRead);
+        }, cancel);
+    }
+
+    public static void ChmodPlusX(string path)
+    {
+        var f = new UnixFileInfo(path);
+        f.FileAccessPermissions |=
+            FileAccessPermissions.UserExecute | FileAccessPermissions.GroupExecute |
+            FileAccessPermissions.OtherExecute;
     }
 }
