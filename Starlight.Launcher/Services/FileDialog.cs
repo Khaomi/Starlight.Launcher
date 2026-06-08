b94ab843705ac22f9198a051e9fa993628cbd377
Rinary <rinary.super@gmail.com>
2026-06-08 16:00:09 +0300

    fix

--- Starlight.Launcher/Services/FileDialog.cs
+++ Starlight.Launcher/Services/FileDialog.cs
@@ -2,6 +2,6 @@ namespace Starlight.Launcher.Services;
 
 public interface IFileDialogService
 {
-    Task<FileResult?> PickFileAsync(string filter, CancellationToken cancel = default);
+    Task<FileResult?> PickFileAsync(string filter = "Content bundles / replays\0*.zip;*.rt\0All Files\0*.*\0\0", CancellationToken cancel = default);
     Task<FileResult?> PickFolderAsync(CancellationToken cancel = default);
 }
